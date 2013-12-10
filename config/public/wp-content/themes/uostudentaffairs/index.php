1c85e33ff934212612b924cac3d73f974d77e8cf
ignition25 <cdjuarez@ucsd.edu>
2013-12-09 16:27:19 -0800

    Removed unused code.

--- config/public/wp-content/themes/uostudentaffairs/index.php
+++ config/public/wp-content/themes/uostudentaffairs/index.php
@@ -1,11 +1,4 @@
 <?php get_header(); ?>
-<?php if ( is_home() ) { ?>
-<?php $title = "RECENT"; ?>
-  <?php } elseif ( is_category() ) { ?> <!-- End index page carousel -->
-    <?php $title = single_cat_title('', false); ?>
-  <?php } else { ?>
-    <?php $title = "OOPS"; ?>
-  <?php } ?>
   <div class="row">
     <div class="col-md-<?php main_content_columns(); ?>">
       <!-- Main content -->
